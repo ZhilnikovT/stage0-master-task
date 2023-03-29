09ac82b46bfea267b1fe6f2f2d07577d414cb18e
ZhilnikovT <126979946+ZhilnikovT@users.noreply.github.com>
2023-03-29 22:19:50 +0300

    Update Power.java

--- src/main/java/com/epam/loops/Power.java
+++ src/main/java/com/epam/loops/Power.java
@@ -1,13 +1,15 @@
 package com.epam.loops;
 
 public class Power {
-
     public void printPower(int numberToPrint, int power) {
-
+        int number = 1;
+        for(int p = 1; p <= power; p++){
+            number *= numberToPrint;
+        }
+        System.out.println(number);
     }
 
     public static void main(String[] args) {
         new Power().printPower(10, 3);
     }
-
 }
