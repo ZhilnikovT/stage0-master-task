7b4a7eff28c9eca93b65e6fb5690b5f0154aa6af
ZhilnikovT <126979946+ZhilnikovT@users.noreply.github.com>
2023-03-29 22:16:09 +0300

    Update DigitsSumCalculator.java

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -1,8 +1,15 @@
 package com.epam.langSyntax;
 
 public class DigitsSumCalculator {
-
-    public void calculateSum(int number) {
+     public static void main(String[] args) {  
+        DigitsSumCalculator n = new DigitsSumCalculator();
+        n.calculateSum(100);
     }
-
+    
+    public void calculateSum(int number) {
+        char[] chars = ("" + number).toCharArray();
+        int rev = Integer.parseInt(String.valueOf(chars[3])) + Integer.parseInt(String.valueOf(chars[2]))
+                + Integer.parseInt(String.valueOf(chars[1])) + Integer.parseInt(String.valueOf(chars[0]));
+        System.out.println(rev);
+    }
 }
